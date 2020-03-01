0aa93aa570c1c36a5ddd91a3a962b1f338546470
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-03-01 15:32:23 +0000

    changed knexfile.js, index.js, connection.js, liste.js, package.json to let heroku host

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,11 @@
+const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 
-const config = require("../knexfile.js");
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
 
-const connection = knex(config);
+const connection = knex(dbConfig);
 
 module.exports = connection;
