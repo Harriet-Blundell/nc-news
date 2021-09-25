b534343682319925756404d8d6c067bb926f1105
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2021-09-25 14:00:56 +0100

    Fix knexfile.js location

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,9 @@
-const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
+const { DATABASE_URL } = process.env;
 
 const dbConfig =
-  ENV === "production"
-    ? { client: "pg", connection: process.env.DATABASE_URL }
-    : require("../knexfile");
+  process.env.NODE_ENV === "production"
+    ? { client: "pg", connection: `${DATABASE_URL}?ssl=true` }
+    : require("../knexfile.js");
 
-const connection = knex(dbConfig);
-
-module.exports = connection;
+module.exports = knex(dbConfig);
